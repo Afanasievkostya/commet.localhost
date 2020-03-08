4f37c971054a9d263a6ffa5cdb1029237a72c147
Afanasiev Konstantin <kost3069@outlook.com>
2020-03-08 17:09:43 +0300

    коммит шестой. Новый файл SQL

--- gb.sql
+++ gb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Фев 24 2020 г., 11:46
+-- Время создания: Мар 08 2020 г., 17:06
 -- Версия сервера: 5.6.43
 -- Версия PHP: 7.1.32
 
@@ -42,17 +42,9 @@ CREATE TABLE `messanges` (
 --
 
 INSERT INTO `messanges` (`id`, `users_id`, `name_user`, `image_user`, `comment`, `data`) VALUES
-(1, 1, 'Юля', 'avatar1.jpg', 'вход1', '10 февраля 2020'),
-(50, 3, 'хаккер', 'avatar2.jpg', 'вход4', '13 февраля 2020'),
-(51, 2, 'Боря', 'avatar3.jpg', 'Прикольная аватарка Юля!', '13 февраля 2020'),
-(54, 17, 'admin', 'no-image.png', 'Привет я ваш администратор!', '14 февраля 2020'),
-(55, 46, 'Дима', 'no-image.png', 'Всем привет я Дима!', '17 февраля 2020'),
-(56, 1, 'Юля', 'avatar1.jpg', 'Привет Дима.', '17 февраля 2020'),
-(57, 59, 'Дима', 'no-image.png', 'Юля привет.', '21 февраля 2020'),
-(58, 4, 'Иван', 'avatar4.jpg', 'Привет Дима', '21 февраля 2020'),
-(59, 2, 'Боря', 'avatar3.jpg', 'Привет всем!', '21 февраля 2020'),
-(60, 3, 'хаккер', 'avatar2.jpg', 'Как ваши дела Дима?', '21 февраля 2020'),
-(61, 4, 'Иван', 'avatar4.jpg', 'Привет Боря.', '21 февраля 2020');
+(87, 1, 'Юля', 'avatar1.jpg', 'Всем привет я Юля.', '08 марта 2020'),
+(88, 1, 'Юля', 'avatar1.jpg', 'Сообщение 1', '08 марта 2020'),
+(89, 71, 'Ира', 'no-image.png', 'Привет я Ира.', '08 марта 2020');
 
 -- --------------------------------------------------------
 
@@ -72,12 +64,14 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `password`, `image`) VALUES
-(1, 'Юля', '$2y$10$uJf4Yzr05nZ7rNWF61VrGeK7SbJqZy8mXscqMv.2drmRj7vWFc/Ra', 'avatar1.jpg'),
-(2, 'Боря', '$2y$10$q31E3EmOBEb6ysZq0/8QAebE5fHG2cGEcnMbt/YkhSI1o8kKAPy9e', 'avatar3.jpg'),
-(3, 'хаккер', '$2y$10$fMN8kC8UY.oaJiEpPcgv2.gfIGKdKlKJq0PeDeP4Bs5njjLeHhTca', 'avatar2.jpg'),
-(4, 'Иван', '$2y$10$JOIxwALVZJtnqySG03UTFezHHPrcx/w.mD7KdyYzMhhnKBCS2pD8C', 'avatar4.jpg'),
-(59, 'Дима', '$2y$10$k9XvAWb3kJs5SZc.JR9ZyeLJszdKxNa1u/OAUMR/8zLyfm3.DWlPC', 'no-image.png'),
-(66, 'admin', '$2y$10$56xPjatqUSbiS6YjBc9Qc.fMmuXlV0mcHPZ5BhubNPPErbZIR2/ju', 'no-image.png');
+(1, '0K7Qu9GP', '$2y$10$uJf4Yzr05nZ7rNWF61VrGeK7SbJqZy8mXscqMv.2drmRj7vWFc/Ra', 'avatar1.jpg'),
+(2, '0JHQvtGA0Y8=', '$2y$10$q31E3EmOBEb6ysZq0/8QAebE5fHG2cGEcnMbt/YkhSI1o8kKAPy9e', 'avatar3.jpg'),
+(3, '0YXQsNC60LrQtdGA', '$2y$10$fMN8kC8UY.oaJiEpPcgv2.gfIGKdKlKJq0PeDeP4Bs5njjLeHhTca', 'avatar2.jpg'),
+(4, '0JjQstCw0L0=', '$2y$10$JOIxwALVZJtnqySG03UTFezHHPrcx/w.mD7KdyYzMhhnKBCS2pD8C', 'avatar4.jpg'),
+(59, '0JTQuNC80LA=', '$2y$10$k9XvAWb3kJs5SZc.JR9ZyeLJszdKxNa1u/OAUMR/8zLyfm3.DWlPC', 'no-image.png'),
+(66, 'YWRtaW4=', '$2y$10$56xPjatqUSbiS6YjBc9Qc.fMmuXlV0mcHPZ5BhubNPPErbZIR2/ju', 'no-image.png'),
+(70, '0KHQsNGI0LA=', '$2y$10$s7L4qI4h0puqBSiVyQPSIuOTcYMVPxHeQEZFs5mNXKLwQ72fPdYQC', 'no-image.png'),
+(71, '0JjRgNCw', '$2y$10$fRylyzEs.NMaNxlpIRCDN..jlo7kppJ7nKGd9uSJu9uDD.BJ89Dw6', 'no-image.png');
 
 --
 -- Индексы сохранённых таблиц
@@ -103,13 +97,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `messanges`
 --
 ALTER TABLE `messanges`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
